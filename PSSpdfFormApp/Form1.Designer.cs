72eb5719fba2af6d460ffe3cc0306123855acf9b
Vepur Saicharan <Vepur.Saicharan@providencesoft.com>
2024-07-26 16:11:50 +0200

    Add project files.

---
+++ PSSpdfFormApp/Form1.Designer.cs
@@ -0,0 +1,150 @@
+﻿namespace PSSpdfFormApp
+{
+    partial class PSSpdfFormApp
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.mnuFile = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuOpen = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveAsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuInfo = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuSign = new System.Windows.Forms.ToolStripMenuItem();
+            this.sealToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.pdfRenderer1 = new PdfiumViewer.PdfViewer();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mnuFile,
+            this.mnuSign,
+            this.sealToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(813, 28);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // mnuFile
+            // 
+            this.mnuFile.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mnuOpen,
+            this.saveAsToolStripMenuItem,
+            this.mnuInfo});
+            this.mnuFile.Name = "mnuFile";
+            this.mnuFile.Size = new System.Drawing.Size(46, 24);
+            this.mnuFile.Text = "&File";
+            // 
+            // mnuOpen
+            // 
+            this.mnuOpen.Name = "mnuOpen";
+            this.mnuOpen.Size = new System.Drawing.Size(185, 26);
+            this.mnuOpen.Text = "&Open";
+            this.mnuOpen.Click += new System.EventHandler(this.mnuOpen_Click);
+            // 
+            // saveAsToolStripMenuItem
+            // 
+            this.saveAsToolStripMenuItem.Name = "saveAsToolStripMenuItem";
+            this.saveAsToolStripMenuItem.Size = new System.Drawing.Size(185, 26);
+            this.saveAsToolStripMenuItem.Text = "Save As";
+            this.saveAsToolStripMenuItem.Click += new System.EventHandler(this.saveAsToolStripMenuItem_Click);
+            // 
+            // mnuInfo
+            // 
+            this.mnuInfo.Name = "mnuInfo";
+            this.mnuInfo.Size = new System.Drawing.Size(185, 26);
+            this.mnuInfo.Text = "Signature &Info";
+            this.mnuInfo.Click += new System.EventHandler(this.mnuInfo_Click);
+            // 
+            // mnuSign
+            // 
+            this.mnuSign.Name = "mnuSign";
+            this.mnuSign.Size = new System.Drawing.Size(52, 24);
+            this.mnuSign.Text = "Sign";
+            this.mnuSign.Click += new System.EventHandler(this.signToolStripMenuItem_Click);
+            // 
+            // sealToolStripMenuItem
+            // 
+            this.sealToolStripMenuItem.Name = "sealToolStripMenuItem";
+            this.sealToolStripMenuItem.Size = new System.Drawing.Size(14, 24);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // pdfRenderer1
+            // 
+            this.pdfRenderer1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pdfRenderer1.Location = new System.Drawing.Point(0, 33);
+            this.pdfRenderer1.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.pdfRenderer1.Name = "pdfRenderer1";
+            this.pdfRenderer1.Size = new System.Drawing.Size(813, 554);
+            this.pdfRenderer1.TabIndex = 2;
+            // 
+            // PSSpdfFormApp
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(813, 587);
+            this.Controls.Add(this.pdfRenderer1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "PSSpdfFormApp";
+            this.Text = "dsa";
+            this.Load += new System.EventHandler(this.PsspdfFormApp_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem mnuSign;
+        private System.Windows.Forms.ToolStripMenuItem mnuFile;
+        private System.Windows.Forms.ToolStripMenuItem mnuOpen;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.ToolStripMenuItem mnuInfo;
+        private System.Windows.Forms.ToolStripMenuItem sealToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveAsToolStripMenuItem;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private PdfiumViewer.PdfViewer pdfRenderer1;
+
+
+    }
+}
+
---
+++ PSSpdfFormApp/Form1.cs
@@ -0,0 +1,234 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Florentis;
+using iTextSharp;
+using iTextSharp.text.pdf;
+using iTextSharp.text.pdf.parser;
+using iTextSharp.text.pdf.interfaces;
+using iTextSharp.text.io;
+using PdfUtils;
+using PdfiumViewer;
+
+
+
+
+namespace PSSpdfFormApp
+{
+    public partial class PSSpdfFormApp : Form
+    {
+        public string defaultPdf = "Empty_PDF.pdf";
+        public bool defaultLoaded = false;
+
+        public string originalFile = "";
+        public string currentFile = "";
+
+        public string name = "Name";
+        public string reason = "Reason";
+
+        private DateTime signatureDateTime;
+        public PSSpdfFormApp()
+        {
+            InitializeComponent();
+        }
+
+        private void loadDefaultPDF()
+        {
+            loadPdf(defaultPdf);
+            defaultLoaded = true;
+        }
+
+        private void loadPdf(string pdfPath)
+        {
+            loadPdf(pdfPath, true);
+        }
+
+        private void loadPdf(string pdfPath, bool changeOriginal)
+        {
+
+            //pdfRenderer1.Load(PdfiumViewer.PdfDocument.Load(pdfPath));
+            pdfRenderer1.Document = PdfiumViewer.PdfDocument.Load(pdfPath);
+            pdfRenderer1.Show();
+
+            currentFile = pdfPath;
+
+            if (changeOriginal)
+            {
+                originalFile = pdfPath;
+            }
+            defaultLoaded = false;
+            this.Text = "PSSpdfFormApp - " + originalFile;
+
+        }
+
+        private void PsspdfFormApp_Load(object sender, EventArgs e)
+        {
+            loadDefaultPDF();
+        }
+
+        private void signToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            sign();
+        }
+
+        public void sign()
+        {
+            SigCtl sigCtl = new SigCtl();
+            sigCtl.Licence = Properties.Settings.Default.License;
+            DynamicCapture dc = new DynamicCaptureClass();
+            DynamicCaptureResult res = dc.Capture(sigCtl, name, reason, null, null);
+
+            if (res == DynamicCaptureResult.DynCaptOK)
+            {
+                SigObj sigObj = (SigObj)sigCtl.Signature;
+                
+
+                String filename = System.IO.Path.GetTempFileName();
+                try
+                {
+                    sigObj.RenderBitmap(filename, 400, 200, "image/png", 0.5f, 0x000000, 0xffffff, 10.0f, 10.0f, RBFlags.RenderOutputFilename | RBFlags.RenderColor32BPP | RBFlags.RenderEncodeData | RBFlags.RenderBackgroundTransparent);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+
+                string newFile = InsertSignatureImageToPdf(filename);
+                loadPdf(newFile, false);
+            }
+        }
+
+        public void save()
+        {
+            save(originalFile);
+        }
+
+        public void save(string path)
+        {
+            FileStream fsOut = new FileStream(path, FileMode.Create, FileAccess.ReadWrite);
+            FileStream fsIn = new FileStream(currentFile, FileMode.Open, FileAccess.Read);
+            fsIn.CopyTo(fsOut);
+
+            fsIn.Close();
+            fsOut.Close();
+        }
+
+        public void saveAs()
+        {
+            saveFileDialog1.Filter = "PDF Documents | *.pdf";
+            if (saveFileDialog1.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                save(saveFileDialog1.FileName);
+                loadPdf(saveFileDialog1.FileName, true);
+            }
+        }
+
+
+        private string InsertSignatureImageToPdf(string imageFileName)
+        {
+            string signedFile = System.IO.Path.GetTempFileName();
+
+            PdfReader reader = new PdfReader(currentFile);
+            using (FileStream fs = new FileStream(signedFile, FileMode.Create, FileAccess.Write))
+            {
+                using (PdfStamper stamper = new PdfStamper(reader, fs))
+                {
+                    PdfContentByte cb = stamper.GetOverContent(1);
+
+                    iTextSharp.text.Image image = iTextSharp.text.Image.GetInstance(imageFileName);
+                    // Change signature points
+                    image.SetAbsolutePosition(90, 220);
+                    image.ScalePercent(30f, 30f);
+
+                    cb.AddImage(image);
+
+                    // Add date and time
+                    cb.BeginText();
+                    cb.SetFontAndSize(BaseFont.CreateFont(BaseFont.HELVETICA, BaseFont.CP1252, BaseFont.NOT_EMBEDDED), 12);
+
+                    // Set the new position for the date and time text
+                    float textX = 90; // Adjust the X coordinate
+                    float textY = 210; // Adjust the Y coordinate
+                    cb.SetTextMatrix(textX, textY);
+
+                    cb.ShowText($"Signed on: {DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss")}");
+                    cb.EndText();
+                }
+            }
+
+            currentFile = signedFile;
+
+            return signedFile;
+        }
+
+        private void getSignatureFromPdf(string pdf_filename)
+        {
+            bool imagesFound = PdfImageExtractor.PageContainsImages(pdf_filename, 1);
+
+            if (imagesFound)
+            {
+                Dictionary<string, System.Drawing.Image> dict = PdfImageExtractor.ExtractImages(pdf_filename, 1);
+                foreach (string key in dict.Keys)
+                {
+                    System.Drawing.Image img = dict[key];
+                    img.Save("sign.png");
+
+                    SigObj sig = new SigObj();
+                    ReadEncodedBitmapResult result = sig.ReadEncodedBitmap("sign.png");
+                    if (result == ReadEncodedBitmapResult.ReadEncodedBitmapOK)
+                    {
+                        MessageBox.Show(sig.Who + " " + sig.Why + " " + sig.When);
+                    }
+                }
+            }
+        }
+
+        private void mnuOpen_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.Filter = "PDF Documents | *.pdf";
+            openFileDialog1.Multiselect = false;
+            DialogResult result = openFileDialog1.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                try
+                {
+                    loadPdf(openFileDialog1.FileName);
+                }
+                catch (FileNotFoundException)
+                {
+                    MessageBox.Show("File not found");
+                }
+                catch (Exception err)
+                {
+                    MessageBox.Show(err.Message);
+                }
+            }
+        }
+
+        private void mnuInfo_Click(object sender, EventArgs e)
+        {
+            frmSignatureInfo info = new frmSignatureInfo();
+            info.loadSignInfo(currentFile);
+            info.ShowDialog();
+
+        }
+
+        private void mnuSave_Click(object sender, EventArgs e)
+        {
+            save();
+        }
+
+        private void saveAsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            saveAs();
+        }
+    }
+}
